a9887bee01fef3b3652c5eee4bdc63ad81e5cc0a
ArtemiyGranat <artemiigranat@gmail.com>
2023-11-04 21:34:46 +0400

    set_max_connections и set_shared_buffers

--- alarmist/src/db/manage.py
+++ alarmist/src/db/manage.py
@@ -9,3 +9,27 @@ async def terminate_process(connection: databases.core.Connection, pid: int):
         WHERE pid = {pid};
         """
     )
+
+
+# TODO(granatam): max_connections is in [1; 262143]. Check this
+async def set_max_connections(
+    connection: databases.core.Connection, max_connections: int
+):
+    await connection.execute(
+        f"ALTER SYSTEM SET max_connections = {max_connections};"
+    )
+    await restart_server(connection)
+
+
+# TODO(granatam): shared_buffers is in [128Kb; ...]. Check this
+async def set_shared_buffers(
+    connection: databases.core.Connection, shared_buffers: int
+):
+    await connection.execute(
+        f"ALTER SYSTEM SET shared_buffers = {shared_buffers};"
+    )
+    await restart_server(connection)
+
+
+async def restart_server(connection: databases.core.Connection):
+    await connection.execute("SELECT pg_reload_conf();")
